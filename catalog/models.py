5a5dd2ed1831a38fea6b38fb4793382d1452ff7e
vtr57 <professorvitorfisica@gmail.com>
2024-06-18 13:49:33 -0300

    add books emprestados.

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,10 @@
 import uuid # Required for unique book instances
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+from django.contrib.auth.models import User
+from datetime import date
+
+
 
 # Create your models here.
 class Genre(models.Model):
@@ -48,6 +52,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -70,6 +76,13 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
     
 class Author(models.Model):
     """Model representing an author."""
