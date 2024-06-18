f88a09e24b5448c0514bd46986bf266af848c196
vtr57 <professorvitorfisica@gmail.com>
2024-06-18 17:47:27 -0300

    add test view.

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -48,7 +48,7 @@ import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User # Required to assign User as a borrower
 
-from catalog.models import BookInstance, Book, Genre, Language
+from catalog.models import BookInstance, Book, Genre
 
 class LoanedBookInstancesByUserListViewTest(TestCase):
     def setUp(self):
@@ -62,13 +62,11 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
         # Create a book
         test_author = Author.objects.create(first_name='John', last_name='Smith')
         test_genre = Genre.objects.create(name='Fantasy')
-        test_language = Language.objects.create(name='English')
         test_book = Book.objects.create(
             title='Book Title',
             summary='My book summary',
             isbn='ABCDEFG',
             author=test_author,
-            language=test_language,
         )
 
         # Create genre as a post-step
@@ -105,3 +103,63 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
 
         # Check we used correct template
         self.assertTemplateUsed(response, 'catalog/bookinstance_list_borrowed_user.html')
+
+        def test_only_borrowed_books_in_list(self):
+            login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+            response = self.client.get(reverse('my-borrowed'))
+
+            # Check our user is logged in
+            self.assertEqual(str(response.context['user']), 'testuser1')
+            # Check that we got a response "success"
+            self.assertEqual(response.status_code, 200)
+
+            # Check that initially we don't have any books in list (none on loan)
+            self.assertTrue('bookinstance_list' in response.context)
+            self.assertEqual(len(response.context['bookinstance_list']), 0)
+
+            # Now change all books to be on loan
+            books = BookInstance.objects.all()[:10]
+
+            for book in books:
+                book.status = 'o'
+                book.save()
+
+            # Check that now we have borrowed books in the list
+            response = self.client.get(reverse('my-borrowed'))
+            # Check our user is logged in
+            self.assertEqual(str(response.context['user']), 'testuser1')
+            # Check that we got a response "success"
+            self.assertEqual(response.status_code, 200)
+
+            self.assertTrue('bookinstance_list' in response.context)
+
+            # Confirm all books belong to testuser1 and are on loan
+            for bookitem in response.context['bookinstance_list']:
+                self.assertEqual(response.context['user'], bookitem.borrower)
+                self.assertEqual('o', bookitem.status)
+
+        def test_pages_ordered_by_due_date(self):
+            # Change all books to be on loan
+            for book in BookInstance.objects.all():
+                book.status='o'
+                book.save()
+
+            login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+            response = self.client.get(reverse('my-borrowed'))
+
+            # Check our user is logged in
+            self.assertEqual(str(response.context['user']), 'testuser1')
+            # Check that we got a response "success"
+            self.assertEqual(response.status_code, 200)
+
+            # Confirm that of the items, only 10 are displayed due to pagination.
+            self.assertEqual(len(response.context['bookinstance_list']), 10)
+
+            last_date = 0
+            for book in response.context['bookinstance_list']:
+                if last_date == 0:
+                    last_date = book.due_back
+                else:
+                    self.assertTrue(last_date <= book.due_back)
+                    last_date = book.due_back
+
